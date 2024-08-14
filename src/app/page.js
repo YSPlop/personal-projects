7e9ee7678582121c2616d621ef79b6096510bfc3
YSPlop <sivarajyukash@gmail.com>
2024-08-14 15:51:36 +1000

    completed image gallery

--- src/app/page.js
+++ src/app/page.js
@@ -17,7 +17,13 @@ function GalleryComponent() {
         <img src="./img1.jpeg" alt="pic1"/>
       </div>
       <div className="gallery-panel">
-        <img src="./img2.jpeg" alt="pic2" />
+        <img src="./img2.jpeg" alt="pic2"/>
+      </div>
+      <div className="gallery-panel">
+        <img src="./img3.jpeg" alt="pic3"/>
+      </div>
+      <div className="gallery-panel">
+        <img src="./img4.jpeg" alt="pic4"/>
       </div>
     </div>
   );
