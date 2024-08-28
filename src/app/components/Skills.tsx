126f6bae05923cfa495adb0c6cfbc58da9a959e5
Yukash <sivarajyukash@gmail.com>
2024-08-28 22:39:15 +1000

    skills done

--- src/app/components/Skills.tsx
+++ src/app/components/Skills.tsx
@@ -1,46 +1,72 @@
-import { FaReact, FaNodeJs, FaAws, FaLaravel } from 'react-icons/fa';
-import '../styles/skills.module.css'
+import React from 'react';
+import Marquee from 'react-marquee-slider';
+import { FaReact, FaNodeJs, FaAws, FaLaravel, FaPhp, FaVuejs, FaLinux, FaAlgolia, FaJs, FaConfluence, FaJira, FaTrello, FaBitbucket, FaPython, FaJava, FaGit, FaGithub } from 'react-icons/fa';
+import useWindowWidth from '../utils/useWiindowWidth';
+
+import styled from 'styled-components';
+
+const StyledMarquee = styled.div<{ $paused?: boolean; $position?: string }>`
+  /* Example styles */
+  display: flex;
+  overflow: hidden;
+
+  /* Apply styles conditionally based on props */
+  ${({ $paused }) => $paused && 'animation-play-state: paused;'}
+  ${({ $position }) => $position && `position: ${$position};`}
+`;
 
 const Skills: React.FC = () => {
+  const skills = [
+    { icon: <FaLinux size={50} className="text-black mx-auto mb-4" />, name: "Linux" },
+    { icon: <FaGit size={50} className="text-orange-500 mx-auto mb-4" />, name: "Git" },
+    { icon: <FaGithub size={50} className="text-black mx-auto mb-4" />, name: "GitHub" },
+
+    { icon: <FaLaravel size={50} className="text-red-500 mx-auto mb-4" />, name: "Laravel" },
+    { icon: <FaAlgolia size={50} className="text-blue-500 mx-auto mb-4" />, name: "Algolia" },
+
+    { icon: <FaPhp size={50} className="text-purple-800 mx-auto mb-4" />, name: "PHP" },
+    { icon: <FaJs size={50} className="text-yellow-500 mx-auto mb-4" />, name: "Javascript" },
+    { icon: <FaPython size={50} className="text-green-500 mx-auto mb-4" />, name: "Python" },
+    { icon: <FaJava size={50} className="text-yellow-500 mx-auto mb-4" />, name: "Java" },
+
+    { icon: <FaVuejs size={50} className="text-green-600 mx-auto mb-4" />, name: "Vue" },
+    { icon: <FaReact size={50} className="text-blue-500 mx-auto mb-4" />, name: "React.js" },
+    { icon: <FaNodeJs size={50} className="text-green-500 mx-auto mb-4" />, name: "Node.js" },
+
+    { icon: <FaConfluence size={50} className="text-blue-500 mx-auto mb-4" />, name: "Confluence" },
+    { icon: <FaJira size={50} className="text-blue-500 mx-auto mb-4" />, name: "Jira" },
+    { icon: <FaTrello size={50} className="text-blue-500 mx-auto mb-4" />, name: "Trello" },
+    { icon: <FaBitbucket size={50} className="text-blue-500 mx-auto mb-4" />, name: "Bitbucket" },
+
+    { icon: <FaAws size={50} className="text-yellow-500 mx-auto mb-4" />, name: "AWS" },
+  ];
+
+  const windowWidth = useWindowWidth();
+
+  // Set different velocities based on screen width
+  const velocity = windowWidth < 768 ? 10 : 25; // Example: 10 for mobile, 25 for larger screens
+
   return (
     <section className="py-20">
       <div className="container mx-auto text-center">
         <h2 className="text-4xl font-bold mb-12">Skills</h2>
-        <div className="overflow-hidden relative">
-          <div className="whitespace-nowrap animate-scroll">
-            <div className="inline-block text-center mx-6">
-              <FaLaravel size={50} className="text-red-500 mx-auto mb-4" />
-              <p className="text-lg">Laravel</p>
-            </div>
-            <div className="inline-block text-center mx-6">
-              <FaReact size={50} className="text-blue-500 mx-auto mb-4" />
-              <p className="text-lg">React.js</p>
-            </div>
-            <div className="inline-block text-center mx-6">
-              <FaNodeJs size={50} className="text-green-500 mx-auto mb-4" />
-              <p className="text-lg">Node.js</p>
-            </div>
-            <div className="inline-block text-center mx-6">
-              <FaAws size={50} className="text-yellow-500 mx-auto mb-4" />
-              <p className="text-lg">AWS</p>
-            </div>
-            {/* Duplicate the items for endless effect */}
-            <div className="inline-block text-center mx-6">
-              <FaLaravel size={50} className="text-red-500 mx-auto mb-4" />
-              <p className="text-lg">Laravel</p>
-            </div>
-            <div className="inline-block text-center mx-6">
-              <FaReact size={50} className="text-blue-500 mx-auto mb-4" />
-              <p className="text-lg">React.js</p>
-            </div>
-            <div className="inline-block text-center mx-6">
-              <FaNodeJs size={50} className="text-green-500 mx-auto mb-4" />
-              <p className="text-lg">Node.js</p>
-            </div>
-            <div className="inline-block text-center mx-6">
-              <FaAws size={50} className="text-yellow-500 mx-auto mb-4" />
-              <p className="text-lg">AWS</p>
-            </div>
+        <div className="flex justify-center items-center overflow-hidden">
+          <div className="w-full">
+            <Marquee
+              velocity={velocity}
+              resetAfterTries={200}
+              scatterRandomly={false}
+              direction="ltr"
+              onInit={() => {}}
+              onFinish={() => {}}
+            >
+              {skills.map((skill, index) => (
+                <div key={index} className="inline-block text-center mx-6">
+                  {skill.icon}
+                  <p className="text-lg">{skill.name}</p>
+                </div>
+              ))}
+            </Marquee>
           </div>
         </div>
       </div>
